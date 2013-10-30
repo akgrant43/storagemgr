f066ab4bad02827839aeff475e2110d635591b81
Alistair Grant <akgrant@gmail.com>
2013-10-30 20:34:25 +1100

    quick_scan: add full scan (rename needed)

--- storagemgr/storage/management/commands/quick_scan.py
+++ storagemgr/storage/management/commands/quick_scan.py
@@ -3,7 +3,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 
-from storage.scan import QuickScan
+from storage.scan import QuickScan, FullScan
 
 from logger import init_logging
 logger = init_logging(__name__)
@@ -19,6 +19,11 @@ class Command(BaseCommand):
             dest='debug',
             default=False,
             help='Load pdb and halt on startup'),
+        make_option('--full',
+            action='store_true',
+            dest='full',
+            default=False,
+            help='Perform full scan instead'),
         )
 
     def handle(self, *args, **options):
@@ -29,7 +34,10 @@ class Command(BaseCommand):
 
         logger.info("Quick Scan starting")
         for rp in args:
-            scan = QuickScan(rp)
+            if options['full']:
+                scan = FullScan(rp)
+            else:
+                scan = QuickScan(rp)
             print("Scanning: {0}".format(scan.root_paths))
             scan.scan()
         logger.info("Quick Scan finished")
