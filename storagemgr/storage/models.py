c32aa95fd03c700c142eda63bdb5e82c5d098aec
Alistair Grant <akgrant@gmail.com>
2013-10-30 11:56:00 +1100

    Collect keywords from Xmp.MicrosoftPhoto.LastKeywordXMP

--- storagemgr/storage/models.py
+++ storagemgr/storage/models.py
@@ -216,6 +216,9 @@ class File(models.Model):
                 keywords = keywords.value
             else:
                 keywords = []
+            xmp = img_exiv2.get('Xmp.MicrosoftPhoto.LastKeywordXMP')
+            if xmp is not None:
+                keywords.extend(xmp)
             keywords = set(keywords)
             old_keywords = self.keyword_set.all()
             # Minimise db lookups by caching in a dictionary
