46ccaa4fee8be213eaea6096a6005832abe16fe1
Alistair Grant <akgrant@gmail.com>
2013-12-11 15:00:34 +1100

    Introduce local_settings.py

--- storagemgr/storagemgr/settings.py
+++ storagemgr/storagemgr/settings.py
@@ -64,8 +64,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'daa',                      # Or path to database file if using sqlite3.
         'USER': 'smgr',                      # Not used with sqlite3.
-        'PASSWORD': 'daafiles',                  # Not used with sqlite3.
-        'HOST': 'alistair-svr',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS': {
             'init_command': 'SET storage_engine=INNODB, SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED'
